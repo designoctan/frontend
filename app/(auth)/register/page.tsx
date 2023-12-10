21a14f1d6fa371d78aa78e1edc6469bd8cc40f21
shivam <chudadsamashivamkumar@gmail.com>
2023-12-10 22:55:10 +0530

    remove image from the login and signup

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -38,7 +38,7 @@ const SignupPage = () => {
         <>
             <div className={containerClassName}>
                 <div className="flex flex-column align-items-center justify-content-center">
-                    <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" />
+                    {/* <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" /> */}
                     <div
                         style={{
                             borderRadius: '56px',
